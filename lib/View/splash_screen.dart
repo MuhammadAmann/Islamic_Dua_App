ef1c03033ca724c6a107fee6262da35631cf4c08
Muhammad Aman <swservices3642@gmail.com>
2023-10-02 15:54:39 +0500

    Project Setup

---
+++ lib/View/splash_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Splash_Screen extends StatefulWidget {
+  const Splash_Screen({super.key});
+
+  @override
+  State<Splash_Screen> createState() => _Splash_ScreenState();
+}
+
+class _Splash_ScreenState extends State<Splash_Screen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(children: [],),
+    );
+  }
+}
