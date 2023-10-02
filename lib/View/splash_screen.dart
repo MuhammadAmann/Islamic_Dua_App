6a79cd52b0630d399e3d7ac8d4c7d85eec879ad6
Muhammad Aman <swservices3642@gmail.com>
2023-10-03 03:05:41 +0500

    Update1

--- lib/View/splash_screen.dart
+++ lib/View/splash_screen.dart
@@ -1,4 +1,7 @@
+import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:islamic_dua_app/View/home.dart';
+import 'package:islamic_dua_app/utils/colors.dart';
 
 class Splash_Screen extends StatefulWidget {
   const Splash_Screen({super.key});
@@ -10,8 +13,28 @@ class Splash_Screen extends StatefulWidget {
 class _Splash_ScreenState extends State<Splash_Screen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(children: [],),
+    return AnimatedSplashScreen(
+      backgroundColor: primarycolor,
+      splash: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Image.asset(
+            "images/dua.png",
+            height: 100,
+            // width: 10,
+          ),
+          Text(
+            "Islamic Dua's",
+            style: TextStyle(
+                color: secondarycolor,
+                fontSize: 36,
+                fontWeight: FontWeight.w600),
+          ),
+        ],
+      ),
+      nextScreen: HomeScreen(),
+      splashIconSize: double.infinity,
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:islamic_dua_app/View/home.dart';
 import 'package:islamic_dua_app/View/splash_screen.dart';
+import 'package:islamic_dua_app/utils/colors.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,6 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Splash_Screen(),
     );
   }
